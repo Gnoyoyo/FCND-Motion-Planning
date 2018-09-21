f20bc3165f555e7dcceb68a21a5563ed11ddffef
Peerarust Siriamphan <peerarust.si@tnis.com>
2018-09-21 13:40:28 +0700

    Completed project

--- README.md
+++ README.md
@@ -60,18 +60,68 @@ The `planning_utils.py` are consist of
 
 #### 1. Set your global home position
 
+##### In the starter code, we assume that the home position is where the drone first initializes, but in reality you need to be able to start planning from anywhere. Modify your code to read the global home location from the first line of the colliders.csv file and set that position as global home (self.set_home_position())
+
+As seen below first read the `colliders_file` which is `colliders.csv` then we assign `lat0` and `lon0` which will be the global home position.
+
+![Code Image](./misc/code/global_home.PNG)
+
 #### 2. Set your current local position
+
+##### In the starter code, we assume the drone takes off from map center, but you'll need to be able to takeoff from anywhere. Retrieve your current position in geodetic coordinates from self._latitude, self._longitude and self._altitude. Then use the utility function global_to_local() to convert to local position (using self.global_home as well, which you just set)
+
+something somwthing something 
+
+![Code Image](./misc/code/local_position.PNG)
 
 #### 3. Set grid start position from local position
 
+##### In the starter code, the start point for planning is hardcoded as map center. Change this to be your current local position
+
+As seen below this will set the grid start point to the drone current local position.
+
+![Code Image](./misc/code/grid_start.PNG)
+
 #### 4. Set grid goal position from geodetic coords
+
+##### In the starter code, the goal position is hardcoded as some location 10 m north and 10 m east of map center. Modify this to be set as some arbitrary position on the grid given any geodetic coordinates (latitude, longitude)
+
+The configuration for the goal position will be on the top of `motion_planning.py` 
+
+![Code Image](./misc/code/grid_goal.PNG)
+
+and the goal will position will be set by using the goal that have been already input at the top of the `motion_planning.py`
+
+![Code Image](./misc/code/grid_goal2.PNG)
 
 #### 5. Modify A* to include diagonal motion (or replace A* altogether)
 
+##### Write your search algorithm. Minimum requirement here is to add diagonal motions to the A* implementation provided, and assign them a cost of sqrt(2). However, you're encouraged to get creative and try other methods from the lessons and beyond!
+
+As seen below this is the added part of diagonal motions and assign them as a cost of sqrt(2).
+
+![Code Image](./misc/code/a_star.PNG)
+
+As seen below this is the `planning_utils.py` which has been already update to support diagonal motions .
+
+![Code Image](./misc/code/a_star2.PNG)
+
 #### 6. Cull waypoints
+
+##### Cull waypoints from the path you determine using search.
+
+As seen below this is the `planning_utils.py` the prune path function is pretty much the same as in lecture.
+
+![Code Image](./misc/code/cull_waypoint.PNG)
 
 ### Execute the flight
 
 #### 1. Does it work ?
+
+##### This is simply a check on whether it all worked. Send the waypoints and the autopilot should fly you from start to goal!
+
+[Video 1](./misc/video/1.mp4)
+
+[Video 2](./misc/video/2.mp4)
 
 It works!
--- motion_planning_udacity.py
+++ motion_planning_udacity.py
@@ -4,24 +4,12 @@ import msgpack
 from enum import Enum, auto
 
 import numpy as np
-import pandas as pd
-import re
 
-from planning_utils import a_star, heuristic, create_grid, prune_path
+from planning_utils import a_star, heuristic, create_grid
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
-
-colliders_file = 'colliders.csv'
-# Goal lon , lat
-goal_lon = -122.40146276
-goal_lat = 37.79774413
-# goal_lon = -122.39223976
-# goal_lat = 37.79335713
-# goal_lon = -122.39623976
-# goal_lat = 37.79635713
-goal_alt = 0
 
 
 class States(Enum):
@@ -132,68 +120,35 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-        print("Read colliders_file")
-        infile = open(colliders_file, 'r')
-        firstLine = infile.readline()
-        s = re.findall(r"[-+]?\d*\.\d+|\d+", firstLine)
-        lat0 = np.float64(s[1])
-        lon0 = np.float64(s[3])
-        print(f"lat0 = {lat0}, lon0 = {lon0}")
-
-        
         
         # TODO: set home position to (lon0, lat0, 0)
-        self.set_home_position(lon0, lat0, 0)
-        print("Home Position Set")
-
-
 
         # TODO: retrieve current global position
-        current_global_position = self.global_position
-        print(current_global_position)
-
  
         # TODO: convert to current local position using global_to_local()
-
-        local_position = global_to_local(self.global_position, self.global_home)
-        print(self.global_position)
+        
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
-
-        print(local_position)
-
         # Read in obstacle map
-        data = np.loadtxt(colliders_file, delimiter=',', dtype='Float64', skiprows=2)
+        data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
         
         # Define a grid for a particular altitude and safety margin around obstacles
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        # grid_start = (-north_offset, -east_offset)
+        grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        grid_start = (int(np.ceil(local_position[0] - north_offset)) , int(np.ceil(local_position[1] - east_offset)))
-
-        print(grid_start)
         
         # Set goal as some arbitrary position on the grid
-        # grid_goal = (-north_offset + 10, -east_offset + 10)
-        
+        grid_goal = (-north_offset + 10, -east_offset + 10)
         # TODO: adapt to set goal as latitude / longitude position and convert
-        global_goal_position = [goal_lon, goal_lat, goal_alt]
-        print(global_goal_position)
-        local_goal_position = global_to_local(global_goal_position, self.global_home)
-        grid_goal = (int(np.ceil(local_goal_position[0] - north_offset)) , int(np.ceil(local_goal_position[1] - east_offset)))
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
-        print("Searching for path")
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
-        path = prune_path(path)
-
-        
         # TODO (if you're feeling ambitious): Try a different approach altogether!
 
         # Convert path to waypoints
